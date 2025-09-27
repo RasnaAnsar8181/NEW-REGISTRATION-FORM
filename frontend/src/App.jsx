8aab299f2192e1237e0ddcb0f9df7265d29cd22a
rasna Ansar <rasnaworkmail@gmail.com>
2025-09-27 09:38:22 +0530

    Create the folder and files for the frontend portion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './Components/Auth/Login'
+import SignUp from './Components/Auth/SignUp'
+import Header from './Components/Header/Header'
+import Dashboard from './Components/Dashboard/Dashboard'
 
 function App() {
-
   return (
     <>
-      <h1>Vite + React</h1>
+      <Router>
+        <Header />
+        <Routes>  
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </Router>
     </>
   )
 }
